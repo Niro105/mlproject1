36e18c5ff1fa35e0068a012bb402da2e519653d2
Niranjan Kumbhar <126443419+Niro105@users.noreply.github.com>
2025-03-12 12:31:33 +0530

    Delete pi.py

--- pi.py
+++
@@ -1,9 +0,0 @@
-import os
-
-file_path = "artifacts\proprocessor.pkl"
-
-if os.path.exists(file_path):
-    print("✅ File exists:", file_path)
-else:
-    print("❌ File NOT found:", file_path)
-    print("🔎 Current working directory:", os.getcwd())
